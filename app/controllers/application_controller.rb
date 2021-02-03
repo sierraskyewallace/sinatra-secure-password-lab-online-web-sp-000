0a26c8a905177c5fb763924c5a9bce369443db2c
Sierra Wallace <sskwallace@gmail.com>
2021-02-03 00:47:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username] == nil && params[:password] == nil
+    if params[:username] == "" && params[:password] == ""
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
